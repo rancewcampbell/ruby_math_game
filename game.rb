ee27a83d5136b0ee4f19b34d6e9fd2a89b512c0d
Rance Campbell <rancewcampbell@gmail.com>
2020-05-25 18:54:27 +0000

    reformat messages

--- game.rb
+++ game.rb
@@ -1,12 +1,13 @@
 class Game
-  attr_reader :player1, :player2, :tester, :testee
-  attr_writer :tester, :testee
+  attr_reader :player1, :player2, :tester, :testee, :round
+  attr_writer :tester, :testee, :round
 
   def initialize
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
     @tester = @player1
     @testee = @player2
+    @round = 1
   end
 
   def set_turns
@@ -23,7 +24,7 @@ class Game
     while player1.lives > 0 && player2.lives > 0 do
       query = Question.new(tester.name)
       Message.score(player1.lives, player2.lives)
-      Message.new_turn
+      Message.new_turn(round)
       query.generate_question
       if query.check_answer
         Message.correct(tester.name)
@@ -32,6 +33,7 @@ class Game
         testee.lose_life
       end
       set_turns
+      self.round += 1
     end
     Message.game_over(testee.name, player1.lives, player2.lives)
   end
--- messages.rb
+++ messages.rb
@@ -11,23 +11,25 @@ class Message
     puts "Good luck!!"
     puts "\n"
     puts "----------------------------------------------------------"
-    puts "\n"
   end
 
   def self.score(p1Lives, p2Lives)
-    puts "P1: #{p1Lives}/3 vs P2 #{p2Lives}/3"
+    puts "\n------- SCORE -------"
+    puts "\nP1: #{p1Lives}/3 vs P2 #{p2Lives}/3"
+    puts "\n"
   end
 
-  def self.new_turn
-    puts "------- NEW TURN -------"
+  def self.new_turn(round)
+    puts "------- ROUND #{round} -------"
+    puts "\n"
   end
 
   def self.correct(name)
-    puts "#{name}: YES! You are correct!"
+    puts "\n#{name}: YES! You are correct!"
   end
 
   def self.wrong(name)
-    puts "#{name}: Not even close..."
+    puts "\n#{name}: Not even close..."
   end
 
   def self.game_over(name, p1Lives, p2Lives)
